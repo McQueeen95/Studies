7b9760ac64669f351d4abc70cd324debb073e7ba
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2023-07-12 15:18:01 +0300

    Update commandLine.py
    3rd and 4th video of command line course (elZero)

--- commandLine.py
+++ commandLine.py
@@ -3,3 +3,9 @@
 # \ : back to root
 # mkdir name1 name2 name3 name4 : make new directories with those names
 # ls : show the files at the current position(lunix) , dir (windous)
+# mv folderName newFolderName : if newFolderName here it move it inside it
+# : if newFolderName not here it renames folderName to newFolderName
+# cp -r folder folder2 : copy folder with its all things to folder2
+# rm -r folder : remove folder and its contents
+
+# (any command) --help : it shows everything about it
