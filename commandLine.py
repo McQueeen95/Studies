e443a542dace6b3e5ac59b5ced4b116bba110f0a
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2023-08-08 04:52:49 +0300

    Update commandLine.py
    notes of video num 5 and 6

--- commandLine.py
+++ commandLine.py
@@ -9,3 +9,13 @@
 # rm -r folder : remove folder and its contents
 
 # (any command) --help : it shows everything about it
+
+# echo : echo "Text" returns "Text"
+#      : echo "Text" > file.txt --> takes the text and puts it in file.txt and discard if there was any text
+#      : echo "Text" >> file.txt --> same but if the file already exists it just append this text
+
+# cat : cat file.txt --> shows the text inside file.txt
+#     : cat file.txt file2.txt file3.txt .. > final.txt--> show their content and puts all of it in final.txt
+#     : or cat * > final.txt
+
+# grep : grep "Text" file.txt it searchs for Text in file.txt
