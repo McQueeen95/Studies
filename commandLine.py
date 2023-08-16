b8210a2ef8547a17f226a572ba286e0c2d46fff0
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2023-08-16 22:30:19 +0300

    Update commandLine.py
    finish of command line elzero course

--- commandLine.py
+++ commandLine.py
@@ -19,3 +19,33 @@
 #     : or cat * > final.txt
 
 # grep : grep "Text" file.txt it searchs for Text in file.txt
+
+# file : file name.txt show to u what file is that
+
+# touch : touch name.txt create file
+
+# move /? : the manual of all things about move
+
+# tree /a : shows to u everything about ur folder and their paths
+
+# osk : on screen keyborad => a visible touch keyboard
+
+# tasklist : open all tasks that running in the background
+
+# taskkill => To end a process by its process ID (PID) : taskkill /pid 1234
+#          => To terminate a process by its image name :  taskkill /im processname.exe
+#          => To forcefully terminate a process without confirmation : taskkill /f /pid 1234
+
+# alias : alias sc = shorcut, this make a short cut for any command u want
+
+# ipconfig : get ur Configuration IP
+
+# command1 && command2 : do both commands
+
+# systeminfo : getting ur ststem info
+
+# ping fast.com > fastPingInfo.txt : getting the ping of fast.com and put in a text file or any
+
+# anycommand | clip : do the command then copy it to your Clipboard
+
+# any command in any OS is on this website (https://ss64.com/)
