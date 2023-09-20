2435dfb57a94f2b3fcd367e3c70cd26e32f6f440
Ahmed Mohamed <ahmedsuberyahooman2592002@gmail.com>
2023-09-21 02:43:40 +0300

    installing numpy library

---
+++ Linear Algebra for ML.py
@@ -0,0 +1,15 @@
+## NumPy to create 2-D arrays
+# [1] NumPy(Numerical Python) is an open-source package that is widely used in science and engineering
+# [2] After this assignment you will be able to:
+#       Use Jupyter Notebook and its features.
+#       Use NumPy functions to create arrays and NumPy array operations.
+#       Use indexing and slicing for 2-D arrays.
+#       Find the shape of an array, reshape it and stack it horizontally and vertically.
+# [3] Jupyter Notebooks are interactive coding journals that integrate live code, explanatory text, equations, visualizations and other multimedia resources, all in a single document
+
+# 1 (Basics of NumPy) : its the main package for scientific computing in python
+
+#   1.1- Packages
+
+import numpy as py
+print(py.__version__)
